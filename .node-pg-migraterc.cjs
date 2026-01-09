f8a01d8c9756aaa40c901c859072d7ffc0c6a266
Dev0071 <lecturergacheru71@gmail.com>
2026-01-09 01:00:17 -0500

    fix: Clean up SSL configuration comments in node-pg-migrate config

--- .node-pg-migraterc.cjs
+++ .node-pg-migraterc.cjs
@@ -1,11 +1,7 @@
 module.exports = {
   databaseUrl: {
     connectionString: process.env.DATABASE_URL,
-    // Enable SSL for Heroku Postgres by default
-    // Heroku requires SSL on all paid tiers (Essential, Premium)
-    ssl: process.env.DATABASE_SSL === 'false' ? false : {
-      rejectUnauthorized: false
-    }
+    ssl: process.env.DATABASE_SSL === 'false' ? false : { rejectUnauthorized: false },
   },
   migrationsTable: 'pgmigrations',
   dir: 'migrations',
